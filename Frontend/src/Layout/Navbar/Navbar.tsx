eb3b2c4eb4f6c9f7a81039abb07ed0ad255b5a11
B3N0I7 <benoit.sixdix@gmail.com>
2024-12-28 20:14:35 +0100

    feat:add-navigation

--- Frontend/src/Layout/Navbar/Navbar.tsx
+++ Frontend/src/Layout/Navbar/Navbar.tsx
@@ -1,15 +1,25 @@
+import { useNavigate } from "react-router";
 import "./navbar.css";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
           <div className="menu-icon"></div>
-          <button type="button">Afficher</button>
-          <button type="button">Ajouter</button>
-          <button type="button">Modifier</button>
-          <button type="button">Quizzer</button>
+          <button type="button" onClick={() => navigate("/display")}>
+            Afficher
+          </button>
+          <button type="button" onClick={() => navigate("/add")}>
+            Ajouter
+          </button>
+          <button type="button" onClick={() => navigate("/modify")}>
+            Modifier
+          </button>
+          <button type="button" onClick={() => navigate("/quiz")}>
+            Quizzer
+          </button>
           <button type="button">SignIn</button>
         </div>
       </nav>
