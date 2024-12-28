1efdb312dfbc313a2482b9f2a5bc2f145c3e1a1b
B3N0I7 <benoit.sixdix@gmail.com>
2024-12-28 19:22:44 +0100

    feat:add-add

--- Frontend/src/App/Routes/constants.ts
+++ Frontend/src/App/Routes/constants.ts
@@ -1,7 +1,9 @@
 import { ROUTE_URL_HOME as HOME } from "./../../pages/Home/constants";
 import { ROUTE_URL_DISPLAY as DISPLAY } from "../../pages/Display/constants";
+import { ROUTE_URL_ADD as ADD } from "../../pages/Add/constants";
 
 export const ROUTE_URL = {
   HOME,
   DISPLAY,
+  ADD,
 };
---
+++ Frontend/src/pages/Add/Add.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "./../../Layout/Layout";
+
+export const Add = () => {
+  return (
+    <Layout>
+      <h1>Ajouter</h1>
+    </Layout>
+  );
+};
