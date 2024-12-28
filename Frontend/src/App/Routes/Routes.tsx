cb6474dfa9e0a36400442b4b26b284e368f39b4f
B3N0I7 <benoit.sixdix@gmail.com>
2024-12-28 19:12:58 +0100

    feat:add-display

--- Frontend/src/App/Routes/Routes.tsx
+++ Frontend/src/App/Routes/Routes.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router";
 import { ROUTE_URL } from "./constants";
 import { Home } from "../../pages/Home";
+import { Display } from "../../pages/Display";
 
 export const PolyglotRoutes = () => {
+  console.log("ROUTE_URL:", ROUTE_URL);
   return (
     <Routes>
       <Route path={ROUTE_URL.HOME} element={<Home />} index />
+      <Route path={ROUTE_URL.DISPLAY} element={<Display />} />
     </Routes>
   );
 };
